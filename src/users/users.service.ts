a12743761f6c47f69429fa57dce36ba4a7b3b6a6
amirHosseinKhademii <sourena2575@gmail.com>
2022-09-16 15:57:39 +0200

    add not found exeption

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -22,14 +22,14 @@ export class UsersService {
 
   async update(id: number, body: Partial<User>) {
     const user = await this.findOne(id);
-    if (!user) throw new Error('User not found');
+    if (!user) throw new NotFoundException('User not found');
     Object.assign(user, body);
     return this.repo.save(user);
   }
 
   async remove(id: number) {
     const user = await this.findOne(id);
-    if (!user) throw new Error('User not found');
+    if (!user) throw new NotFoundException('User not found');
     return this.repo.remove(user);
   }
 }
