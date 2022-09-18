197fe9770ffd848a0c841ff89d63383fd15c96f2
amirHosseinKhademii <sourena2575@gmail.com>
2022-09-18 11:00:14 +0200

    Make auth test for sign up

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('AUTH SERVICE', () => {
   const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+    find: (): Promise<User[]> =>
+      Promise.resolve([
+        //{ id: 20, email: 'test', password: 'test' }
+      ]),
+    create: (email: string, password: string): Promise<User> =>
+      Promise.resolve({ id: 10, email, password }),
   };
 
   let service: AuthService;
@@ -26,5 +30,16 @@ describe('AUTH SERVICE', () => {
 
   it('Create an instanse of auth service', async () => {
     expect(service).toBeDefined();
+  });
+  it('Sign up should work as expected', async () => {
+    const email = 'amir@gmail.com';
+    const password = '12345';
+    const user = await service.signup(email, password);
+    const [salt, hash] = user.password.split('.');
+    expect(user.email).toBe(email);
+    expect(user.password).not.toBe(password);
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+    expect(user.id).toBe(10);
   });
 });
